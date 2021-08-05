5c34e5377a0035454058c4f5983be6f8eca78230
hermeling.andreas@guj.de <hermeling.andreas@guj.de>
2021-08-05 07:52:13 +0000

    end-of-materialization-practice

--- models/marts/core/int_orders__pivoted.sql
+++ models/marts/core/int_orders__pivoted.sql
@@ -11,7 +11,7 @@ pivoted as (
         {%- for payment_method in payment_methods -%}
         sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount
         {% if not loop.last %},{%- endif -%}
-        {%- endfor -%}
+        {%- endfor %}
     from payments
     where status = 'success'
     group by 1
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,13 +1,12 @@
 select
-id as payment_id,
-orderid as order_id,
-paymentmethod as payment_method,
-status,
-
--- amount is stored in cents, convert it to dollars
-{{ cents_to_dollars('amount', 2) }} amount,
-created as created_at
-
-from {{ source('stripe', 'payment') }}
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    -- amount is stored in cents, convert it to dollars
+    {{ cents_to_dollars('amount', 2) }} amount,
+    created as created_at
+from 
+    {{ source('stripe', 'payment') }}
 
 {{ limit_data_in_dev('_batched_at', 1) }}
